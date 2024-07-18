410d799c9c5f278304e6e614c1f371f0a4c59423
ulugmurod khurramov <abrormurtozayevich@gmail.com>
2024-07-18 23:37:09 +0900

    karochi

---
+++ src/components/motor/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../navbar';
+import Wallpaper from '../wallpaper';
+import Shop from '../shop';
+import Futer from '../futer';
+
+const MotorComponents = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Wallpaper/>
+        <Shop/>
+        <Futer/>
+    </div>
+  );
+};
+
+export default MotorComponents;
+
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div><H1>Camper</H1></div>
+        <div>
+            <p>Motor</p>
+            <p>Caravan</p>
+            <p>Tuning</p>
+            <p>Used Car</p>
+            <p>Camping Place</p>
+        </div>
+        <div></div>
+
+
+
+    </div>
+  );
+};
+
+export default Navbar;
